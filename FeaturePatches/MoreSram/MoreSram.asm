b7ad2884b43f77d4da98f8a1b924bfb100be4b08
xragey <github@ragey.net>
2020-10-14 17:51:33 +0200

    Support Lunar Magic 3.20
    Fixes an issue that allowed you to "leak" some data from one save file to another in the file menu. Also adds support for the new method Lunar Magic 3.20 implements regarding the Sprite 19 fix.

--- FeaturePatches/MoreSram/MoreSram.asm
+++ FeaturePatches/MoreSram/MoreSram.asm
@@ -1,22 +1,21 @@
-; ----------------------------------------------------------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ;
-; "More Sram"
+; MoreSram.asm
 ; by Ragey <i@ragey.net>
 ; https://github.com/xragey/smw
 ;
-; Increases the SRAM capacity of Super Mario World from 140 bytes to 339 bytes by extending the RAM area that is transferred to and
-; from SRAM when saving or loading a game. This patch differs from other patches in that it retains the original game's logic in
-; handling SRAM and does not expand the physical SRAM area. This means that this solution is compatible with any emulator or hard-
-; ware solution.
+; Increases the SRAM capacity of Super Mario World to 339 bytes (up from 140) by
+; extending the length of the RAM array that is transferred to and from SRAM.
 ;
-; Installing this patch reclaims offset $7E1F49 (141 bytes), which (with default settings) is entirely located within the SRAM area.
+; This patch differs from similar patches in that it retains the original game's
+; logic in handling saving and loading, which means it should be compatible with
+; all emulators and most other patches.
 ;
-; Note that this patch does not clean up the other uses of RAM within the new SRAM area. This results in some notable side-effects,
-; such as the "collected all Dragon Coins" flag now also saving to SRAM, as these flags are coincidentally stored within the new
-; SRAM area. Many other addresses in this area are useless to transfer to SRAM, so to take full advantage of the added SRAM, some
-; manual remapping of addresses is needed.
+; Installing this patch frees $7E1F49 (141 bytes).
 ;
-; ----------------------------------------------------------------------------------------------------------------------------------
+; See README.md for more (technical) information.
+;
+;-------------------------------------------------------------------------------
 
 @asar 1.71
 
@@ -26,13 +25,14 @@
 !addr = $0000
 !long = $000000
 
-; ----------------------------------------------------------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 
 ; Transfer range, excluding 2 checksum bytes.
 !cSramSize = 339
 
 ; !cSramSize bytes
-; Ram area that is transferred to SRAM. Does not need to be equal to !OverworldState.
+; Ram area that is transferred to SRAM. Does not need to be equal to
+; !OverworldState.
 !TransferableRam = $1EA2|!addr
 
 ; 141 bytes
@@ -41,62 +41,67 @@
 
 ; 6*(!cSramSize+2) bytes
 ; SRAM array, divided in six blocks of !cSramSize bytes per block.
-if !sa1
-{
-	!Sram = $41C000
-}
+!Sram = select(!sa1, $41C000, $700000)
+
+;-------------------------------------------------------------------------------
+
+; Initial save data hijack. Save any change on the overworld editor to install.
+assert read1($009F19) == $22, "Missing required Lunar Magic hijack"
+
+; Sprite 0x19 fix. This fix differs between Lunar Magic versions. Supports both
+; the old and new methods. Save any change on the overworld editor to install.
+if read1($01E762) == $22
+	!cLMSprite19Fix = $01E763
+elseif read1($01E763) == $22
+	!cLMSprite19Fix = $01E764
 else
-{
-	!Sram = $700000
-}
+	assert 0, "Missing required Lunar Magic hijack"
 endif
 
-; ----------------------------------------------------------------------------------------------------------------------------------
+; ------------------------------------------------------------------------------
 
-assert read1($009F19) == $22, "Missing required Lunar Magic hijack"
-assert read1($01E762) == $22, "Missing required Lunar Magic hijack"
+; Bypass $1F49 buffer.
+org $009BDF : dw !TransferableRam
+org $009D19 : dw !TransferableRam
+org $009F17 : dw !OverworldState
+org $009F23 : dw !OverworldState+111 ; $1F11-$1F26
+org $00A195 : rts
+org $048F94 : nop #11
+org $048FA9 : nop #24
+org $048FC8 : nop #6
+org $048FD6 : nop #6
+org $049041 : nop #11
+org read3($009F1A) : skip 11 : dw !OverworldState
 
-; ----------------------------------------------------------------------------------------------------------------------------------
+; Lunar Magic sprite 19 fix.
+if !cLMSprite19Fix == $01E763
+	org read3($01E763) : skip 7 : nop #3
+elseif !cLMSprite19Fix == $01E764
+	org read3($01E764) : skip 12 : nop #3
+endif
 
-; Bypass $1F49 buffer
-org $009BDF|!long : dw !TransferableRam
-org $009D19|!long : dw !TransferableRam
-org $009F09|!long : dw !OverworldState-1
-org $009F17|!long : dw !OverworldState
-org $009F23|!long : dw !OverworldState+111 ; $1F11-$1F26
-org $00A195|!long : rts
-org $048F94|!long : nop #11
-org $048FA9|!long : nop #24
-org $048FC8|!long : nop #6
-org $048FD6|!long : nop #6
-org $049041|!long : nop #11
+; File offsets (assuming length 339+2).
+org $009CCB : db $00, (!cSramSize+2)>>8, ((!cSramSize+2)*2)>>8
+org $009CCE : db $00, (!cSramSize+2), (!cSramSize+2)*2
 
-; Lunar Magic $1F49
-org read3($009F1A)|!long : skip 11 : dw !OverworldState
-org read3($01E763)|!long : skip 7 : nop #3
+; File length.
+org $009BF1 : dw !cSramSize
+org $009D1E : dw !cSramSize
 
-; File offsets (assuming length 339+2)
-org $009CCB|!long : db $00, (!cSramSize+2)>>8, ((!cSramSize+2)*2)>>8
-org $009CCE|!long : db $00, (!cSramSize+2), (!cSramSize+2)*2
+; File erasing routine.
+org $009B54 : dw !cSramSize+2
+org $009B5C : sta.l !Sram+((!cSramSize+2)*3),x
 
-; File length
-org $009BF1|!long : dw !cSramSize
-org $009D1E|!long : dw !cSramSize
+; File checksum routine.
+org $009DC1 : dw (!cSramSize+2)*3
+org $009DC6 : lda.l !Sram+!cSramSize,x ; 70 01 53
+org $009DCF : dw !cSramSize
+org $009DEE : dw ((!cSramSize+2)*3)-1
 
-; File erasing routine
-org $009B54|!long : dw !cSramSize+2
-org $009B5C|!long : sta.l !Sram+((!cSramSize+2)*3),x
-
-; File checksum routine
-org $009DC1|!long : dw (!cSramSize+2)*3
-org $009DC6|!long : lda.l !Sram+!cSramSize,x ; 70 01 53
-org $009DCF|!long : dw !cSramSize
-org $009DEE|!long : dw ((!cSramSize+2)*3)-1
-
-; Restore backup copy
-org $009C02|!long : dw (!cSramSize+2)*3
-org $009C08|!long : dw (!cSramSize+2)*2
-org $009CF7|!long : autoclean jml EmplaceBackup
+; Restore backup copy.
+org $009C02 : dw (!cSramSize+2)*3
+org $009C08 : dw (!cSramSize+2)*2
+org $009CF7 : autoclean jml EmplaceBackup
 
 freecode
 EmplaceBackup:
@@ -118,14 +123,23 @@ EmplaceBackup:
 	plx
 	jml $009D11|!long
 
-; Exit counter on file select
+; File clearing routine (called when switching files on the menu).
+org $009F06 : autoclean jml ClearFile
+
+freecode
+ClearFile:
+	rep #$10
+	ldx #(!cSramSize-2)
+-	stz !TransferableRam,x
+	dex
+	bpl -
+	sep #$10
+	jml $009F0E|!long
+
+; Exit counter on file select.
 org $009D66
 if !TransferableRam < !OverworldState
-{
 	lda.l !Sram+140+(!OverworldState-!TransferableRam),x
-}
 else
-{
 	lda.l !Sram+140-(!TransferableRam-!OverworldState),x
-}
 endif
--- FeaturePatches/MoreSram/README.md
+++ FeaturePatches/MoreSram/README.md
@@ -1,17 +1,20 @@
 # More SRAM
 
-Increases the SRAM capacity of Super Mario World from 140 bytes to 339 bytes by extending the RAM area that is transferred to and
-from SRAM when saving or loading a game. This patch differs from other patches in that it retains the original game's logic in
-handling SRAM and does not expand the physical SRAM area. This means that this solution is compatible with any emulator or hardware
-solution.
+Increases the SRAM capacity of Super Mario World to 339 bytes (up from 140) by
+extending the length of the RAM array that is transferred to and from SRAM.
+
+This patch differs from similar patches in that it retains the original game's
+logic in handling saving and loading, which means it should be compatible with
+all emulators and most other patches.
 
 ## Reclaimed RAM
 
-Installing this patch reclaims offset $7E1F49 (141 bytes), which (with default settings) is entirely located within the SRAM area.
+Installing this patch reclaims $7E1F49 (141 bytes).
 
 ## Notes
 
-Note that this patch does not clean up the other uses of RAM within the new SRAM area. This results in some notable side-effects,
-such as the "collected all Dragon Coins" flag now also saving to SRAM, as these flags are coincidentally stored within the new SRAM
-area. Many other addresses in this area are useless to transfer to SRAM, so to take full advantage of the added SRAM, some manual
-remapping of addresses is needed.
+This patch was tested with Lunar Magic 3.20.
+
+Keep in mind that, with default settings, this patch frees area ($1F49-$1FD5)
+but transfers area ($1EA2-$1FF5) to and from SRAM. Any other vanilla uses of
+this range are not cleaned up by this patch.

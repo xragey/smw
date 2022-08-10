a3a870faeabe77b15a538dec2a23756cc61a6c95
Ragey <46693850+xragey@users.noreply.github.com>
2022-08-10 11:25:00 +0200

    Update item memory patch

--- FeaturePatches/ItemMemory/ItemMemory.asm
+++ FeaturePatches/ItemMemory/ItemMemory.asm
@@ -1,70 +1,74 @@
-; ----------------------------------------------------------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ;
-; "Item Memory"
+; Item Memory
 ; by Ragey <i@ragey.net>
 ; https://github.com/xragey/smw
-; 
-; Replaces the item memory system in Super Mario World with a different system that assigns a bit to every individual tile in a
-; stage, rather than having columns within a screen share the same bit. Also implements item memory settings 3. Implements a flag
-; that can be used to toggle the use of item memory. Compatible with the ExLevel system implemented by recent versions of Lunar
-; Magic.
 ;
-; Installing this patch reclaims offset $7E19F8 (384 bytes).
+; Replaces the item memory system in Super Mario World with a different system
+; that assigns a bit to every individual tile, rather than having tiles share a
+; bit for each column per screen. Also implements item memory setting 3. This is
+; compatible with recent versions of Lunar Magic that implement ExLevel.
 ;
-; Division and multiply routines by GreenHammerBro <https://smwc.me/u/18802>
-; Additional coding by lx5 <https://github.com/TheLX5>
+; Installing this patch frees 384 bytes of ram at $7E19F8.
+; For more information, review the README.
 ;
-; ----------------------------------------------------------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 
-@asar 1.71
+@asar 1.81
 
-!sa1  = 0
-!fast = 0
-!bank = $00
-!addr = $0000
-!long = $000000
+assert read1($00FFD5) != $23, "This patch does not support SA-1 images."
 
-; ----------------------------------------------------------------------------------------------------------------------------------
+if read1($00FFD5)&$10 == $10
+	!sa1 = 0
+	!fast = 1
+else
+	!sa1 = 0
+	!fast = 0
+endif
 
-; 2 bytes
-; Screen size as defined by ExLevel specification.
-!ExLevelScreenSize = $13D7|!addr
+!bank = select(!fast, $80, $00)
+!long = select(!fast, $800000, $000000)
+!addr = select(!sa1, $6000, $0000)
+
+;-------------------------------------------------------------------------------
 
 ; 1 byte
-; Toggles the use of item memory.
-; ------r- : Disable reading (everything will always respawn).
-; -------w : Disable writing.
-!ItemMemoryMask = $7F9C7B
+; Toggles the use of item memory. It's up to you to set and clear it.
+; The default address ($0DDB) is cleared on reset only.
+; ------r-: Disable reading (everything will always respawn).
+; -------w: Disable writing.
+!ItemMemoryMask = $0DDB|!addr
 
 ; 7168 bytes
-; Item memory, divided in four blocks of 1792 bytes per block.
-!ItemMemory = $7F9C7C
+; Item memory, divided in four blocks of 1792 bytes per setting.
+!ItemMemory = $7F9C7B
 
-; ----------------------------------------------------------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 
-; Loading level code
+; Loading level code.
 org $0096F4|!long
-	autoclean jsl ClearItemMemory
-	
-freecode
+StageLoad:
+	jsl ClearItemMemory
+
+; Item memory offsets, for settings 0,1,2,3 respectively.
+org $00BFFF
+ItemMemoryBlockOffsets:
+    dw $0000, $0700, $0E00, $1500
+
+; Write item memory routine. Retains the vanilla location to allow for backward
+; compatibility with vanilla and existing custom resources.
+org $00C00D
+WriteItemMemoryBank0:
+	autoclean jsl WriteItemMemory
+	rts
+
+; Clear all item memory. This is called from the level loading code (see above).
 ClearItemMemory:
+	; Check if we're entering from the overworld.
 	lda $141A|!addr
 	bne +
-	
-if !sa1
-{
-	; rather crude, could be optimized
-	rep #$30
-	ldx #$1C00
-	lda #$0000
--	dex
-	dex
-	sta !ItemMemory,x
-	bpl -
-	sep #$30
-}
-else
-{
+
+	; Clear !ItemMemory.
 	rep #$30
 	lda.w #!ItemMemory
 	sta.w $2181
@@ -73,214 +77,171 @@ else
 	sta.w $2183
 	ldx #$8008
 	stx $4300
-	ldx #GetMemory_indices ; address of a zero byte
+	ldx #ItemMemoryBlockOffsets
 	stx $4302
-	lda #GetMemory_indices>>16 ; address of a zero byte
+	lda #ItemMemoryBlockOffsets>>16
 	sta $4304
 	ldx #$1C00
 	stx $4305
 	lda #$01
 	sta $420B
 	sep #$10
-}
-endif
 
-+	jml $05D796|!long ; restored code
++	; Restore overwritten jump position.
+	jml $05D796|!long
+warnpc $00C063
 
-; Coin object creation routine
-org $0DA8D3|!long
-	sta $01
-	jsr $A6B1
-SingleTile:
-	cpx #$04
-	bne GenerateTile
-	phx
-	phy
-	lda $1BA1|!addr
-	sta $9B ; ---xxxxx : horizontal screen number
-	tya
-	and #$0F
-	asl #4
-	sta $9A ; xxxx---- : x-position, low
-	pei ($00)
-	pei ($02)
-	rep #$20
-	lda $57
-	and #$00F0
-	clc 
-	adc $6B
-	sec 
-	sbc #$C800
-	sta $00
-	lda !ExLevelScreenSize
-	sta $02
-	jsl Divide
-	lda $02
-	sta $98
-	pla
-	sta $02
-	pla 
-	sta $00
-	sep #$20
-	autoclean jsl ReadItemMemory
-	ply
-	plx
-	jmp $A924
-	warnpc $0DA924|!long
-
-org $00C00D|!long
-	jsl WriteItemMemory
-	rts
-
-org $00C020|!long
-	dl WriteItemMemory
-	dl ReadItemMemory
-
-org $0DA92E|!long
-GenerateTile:
-
-org $0DA947|!long
-	bpl SingleTile
-
-org $0DA957|!long
-	jmp SingleTile
-
+; WriteItemMemory. Marks a certain coordinate as collected.
+; This can be used as a shared routine.
+; On entry, $98 should be set to the X position and $9A as the Y position.
 freecode
-Divide:
-	asl $00
-	ldy #$0F
-	lda #$0000
--	rol a
-	cmp $02
-	bcc +
-	sbc $02
-+	rol $00
-	dey
-	bpl -
-	sta $02
-	rtl
-
-; Specialized routine, do not reuse elsewhere
-Multiply:
-	rep #$20
-	ldy $45
-	sty $4202
-	ldy $47 
-	sty $4203
-	stz $4B
-	ldy $48
-	lda $4216
-	sty $4203
-	sta $49
-	lda $4A
-	rep #$11
-	adc $4216
-	ldy $46
-	sty $4202
-	sep #$10
-	clc
-	ldy $48
-	adc $4216
-	sty $4203
-	sta $4A
-	lda $4B
-	clc
-	adc $4216
-	sta $4B
-	sep #$20
-	rtl
-
 WriteItemMemory:
 	lda !ItemMemoryMask
 	bit #$01
-	bne +
-	phx
-	phy
+	bne .Return
+
+	; X = Item memory index
+	lda $13BE
+	asl
+	tax
+
+	; $45 = $13D7 * X position
+	rep #$30
+	ldy $13D7|!addr
+	lda $9A
+	lsr #4
+	pha
+	lsr #4
+	cmp $13D7|!addr
+	bcs +
+	tay
+	lda $13D7|!addr
++	sep #$30
+	sta $211B
+	xba
+	sta $211B
+	sty $211C
 	rep #$20
-	jsr GetMemory
+	lda $2134
+	sta $45
+
+	; $47 = Y positon * 16
+	lda $98
+	and #$3FF0
+	sta $47
+
+	; A = Absolute offset
+	pla
+	and #$000F
+	clc
+	adc $45
+	clc
+	adc $47
+	clc
+	adc.l ItemMemoryBlockOffsets,x
+
+	; X = Address offset
+	; A = Bit to set in address
+	rep #$10
+	sta $45
+	lsr #3
+	tax
+	phx
+	lda $45
+	and #$0007
+	tax
 	sep #$20
+	lda.l $00C0AA|!bank,x
+	plx
 	ora.l !ItemMemory,x
 	sta.l !ItemMemory,x
 	sep #$10
-	ply
-	plx
-+	rtl
 
+.Return
+	rtl
+
+; ReadItemMemory. Checks if the current block coordinate is marked as collected.
+; This can be used as a shared (object generation) routine.
+; On entry, $6B+Y should be set to the current block linear index. For pretty
+; much all object generation routines, this is already set correctly. Returns
+; A=$00 if the flag is not set or any other value if it's set.
 ReadItemMemory:
 	lda !ItemMemoryMask
 	bit #$02
-	bne +
-	phx
-	phy
-	rep #$20
-	jsr GetMemory
-	sep #$20
-	lda.l !ItemMemory,x
-	and $49
-	sta $0F
-	sep #$10
-	ply
-	plx
-+	rtl
+	beq +
+	lda #$00
+	rtl
 
-; Returns target address in X (16 bit) and bit to set in $49
-GetMemory:
-	lda $5D
-	and #$003F
-	asl #4
-	sta $45 ; stage width in increments of 16 pixels (e.g. blocks)
-
-	lda $98
-	and #$3FF0
-	lsr #4
-	sta $47 ; y position
-
-	rep #$10
-	tsx
-	cpx #$2000
-	sep #$30
-	bcs .sa1
-
-.snes
-	jsl Multiply ; result $49 = y position * stage width
-	bra +
-
-.sa1
-	lda.b #Multiply
-	sta $0183
-	lda.b #Multiply>>8
-	sta $0184
-	lda.b #Multiply>>16
-	sta $0185
-	lda #$D0
-	sta $2209
--	lda $018A
-	beq -
-	stz $018A
-+	rep #$30
-	lda $13BE|!addr
-	and #$0003
-	tax ; item memory offset
-	lda $9A : and #$3FF0 : lsr #4
-	clc
-	adc $49
-	sta $49 ; id assigned to current location
-	lsr #3
-	clc
-	adc.l .indices,x
-	pha ; target address (16 bit)
-	sep #$30
-	lda $49
-	and #$07
++	; A = $45 = Absolute offset
+	lda $13BE
+	asl
 	tax
-	lda.l .bits,x
-	sta $49 ; bit to set in target address
-	rep #$10
-	plx ; target address (16 bit)
-	rts
+	rep #$30
+	lda $6B
+	sec
+	sbc #$C800
+	clc
+	adc.l ItemMemoryBlockOffsets,x
+	sta $45
+	tya
+	clc
+	adc $45
+	sta $45
 
-.indices
-    dw $0000, $0700, $0E00, $1500
+	; X = Address offset
+	; A = Bit to read in address
+	lsr #3
+	tax
+	phx
+	lda $45
+	and #$0007
+	sep #$20
+	tax
+	lda.l $00C0AA|!bank,x
+	plx
+	and.l !ItemMemory,x
+	sep #$10
 
-.bits
-    db $01, $02, $04, $08, $10, $20, $40, $80
+.Return
+	rtl
+
+; Object creation routine for standard objects $01-$0E and tileset object $31.
+; Of these, object $05 (regular coin) uses item memory by default.
+; (POINT OF ADVICE: There is leftover space here for you to add checks to have
+; some of the others use item memory as well!)
+org $0DA8D8
+Object01:
+	cpx #$04
+	beq +
+	jmp $A92E
++	phx
+	jsl ReadItemMemory
+	beq +
+	plx
+	jmp $A928
++	plx
+	jmp $A92E
+warnpc $0DA92B
+
+; Object creation routine for extended objects $10-$40. Some of these use item
+; memory by default.
+org $0DA5F0
+ExObject10:
+	phx
+	jsl ReadItemMemory
+	beq +
+	plx
+	jmp $A63D
++	plx
+	jmp $A648
+warnpc $0DA63C
+
+; Object creation routine for extended object $41 (Yoshi coin).
+org $0DB2E0
+ExObject41:
+	ldy $57
+	jsl ReadItemMemory
+	bne +
+	jmp $B322
++	rts
+warnpc $0DB321
--- FeaturePatches/ItemMemory/README.md
+++ FeaturePatches/ItemMemory/README.md
@@ -1,14 +1,23 @@
 # Item Memory
 
-Replaces the item memory system in Super Mario World with a different system that assigns a bit to every individual tile in a stage,
-rather than having columns within a screen share the same bit. Also implements item memory settings 3. Implements a flag that can be
-used to toggle the use of item memory. Compatible with the ExLevel system implemented by recent versions of Lunar Magic.
+Replaces the item memory system in Super Mario World with a different system
+that assigns a bit to every individual tile in a stage, rather than having the
+columns of a (sub)screen share the same bit. Also implements item memory 3, and
+implements an independent flag that can be used to toggle the use of item
+memory. Compatible with the ExLevel system implemented by recent versions of
+Lunar Magic.
 
-## Reclaimed RAM
+This patch frees RAM offset `$7E19F8` (384 bytes), but requires you to assign a
+block of 7169 bytes elsewhere in RAM. The patch uses long addressing for this,
+so this block can be anywhere in the `$7Exxxx` or `$7Fxxxx` range.
 
-Installing this patch reclaims offset $7E19F8 (384 bytes).
+ASM programmers can leverage the `ReadItemMemory` and `WriteItemMemory` routines
+in this patch to have other stuff hook into and use item memory as well. Review
+the source code for details.
 
-## Acknowledgements
+Special thanks to GreenHammerBro <https://smwc.me/u/18802> for his documentation
+on the (Ex)Level file format.
 
-Division and multiply routines by GreenHammerBro <https://smwc.me/u/18802>
-Additional coding by lx5 <https://github.com/TheLX5>
+**Note:** This is a rewrite of a patch that I have been hosting here since early
+2020 to improve performance and code clarity. If you need the old version,
+review the git history.
